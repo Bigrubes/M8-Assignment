82d99fa52f486913d46df51968b66a9f3f3cde47
Reuben Ramos <rsramos23@gmail.com>
2023-11-18 13:50:23 -0800

    completed steps 4-8

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -11,26 +11,47 @@
 
 // console.log(movies[0])
 //STEP 3
-let movies = new Array(5)
-movies[0] = 'Jurassic Park'
-movies[1] = 'Back to the Future'
-movies[2] = 'The Rock'
-movies[3] = 'Ponyo'
-movies[4] = 'Spiderman across the Spiderverse'
+// let movies = new Array(5)
+// movies[0] = 'Jurassic Park'
+// movies[1] = 'Back to the Future'
+// movies[2] = 'The Rock'
+// movies[3] = 'Ponyo'
+// movies[4] = 'Spiderman across the Spiderverse'
 
-let newMovies = movies.splice(2,0,'Top Gun: Maverick') 
+// let newMovies = movies.splice(2,0,'Top Gun: Maverick') 
 
-console.log(movies.length)
+// console.log(movies.length)
 //STEP 4
-
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse']
+// delete movies[0]
+// console.log(movies)
 //STEP 5
-
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// for (let movie in movies){
+//     console.log(movies[movie])
+// }
 //STEP 6
-
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// for (let movie of movies){
+//     console.log(movie)
+// }
 //STEP 7
-
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// for (let movie of movies.sort()){
+//     console.log(movie)
+// }
 //STEP 8
-
+let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+console.log(`Movies I Like: \n \n`)
+for (let movie of movies.sort()){
+    console.log(movie)
+}
+console.log(`\n \n`)
+console.log(`Movies I regret watching: \n \n`)
+for (let leastFavMovie of leastFavMovies.sort()){
+    console.log(leastFavMovie)
+}
 //STEP 9
 
 //STEP 10
