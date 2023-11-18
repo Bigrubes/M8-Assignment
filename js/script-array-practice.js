992f62de4ac9187217a019095fea02e31281bad4
Reuben Ramos <rsramos23@gmail.com>
2023-11-18 12:49:13 -0800

    completed steps 1-3

---
+++ js/script-array-practice.js
@@ -0,0 +1,50 @@
+//STEP 1
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse']
+// console.log(movies[1])
+//STEP 2
+// let movies = new Array(5)
+// movies[0] = 'Jurassic Park'
+// movies[1] = 'Back to the Future'
+// movies[2] = 'The Rock'
+// movies[3] = 'Ponyo'
+// movies[4] = 'Spiderman across the Spiderverse'
+
+// console.log(movies[0])
+//STEP 3
+let movies = new Array(5)
+movies[0] = 'Jurassic Park'
+movies[1] = 'Back to the Future'
+movies[2] = 'The Rock'
+movies[3] = 'Ponyo'
+movies[4] = 'Spiderman across the Spiderverse'
+
+let newMovies = movies.splice(2,0,'Top Gun: Maverick') 
+
+console.log(movies.length)
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
