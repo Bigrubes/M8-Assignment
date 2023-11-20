58d37dfa588dbd28f35ba8f43381633a99d9398c
Reuben Ramos <rsramos23@gmail.com>
2023-11-19 22:46:46 -0800

    completed practice 1-17

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -41,31 +41,82 @@
 //     console.log(movie)
 // }
 //STEP 8
-let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
-let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
-console.log(`Movies I Like: \n \n`)
-for (let movie of movies.sort()){
-    console.log(movie)
-}
-console.log(`\n \n`)
-console.log(`Movies I regret watching: \n \n`)
-for (let leastFavMovie of leastFavMovies.sort()){
-    console.log(leastFavMovie)
-}
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+// console.log(`Movies I Like: \n \n `)
+// for (let movie of movies.sort()){
+//     console.log(movie)
+// }
+// console.log(`\n `)
+// console.log(`Movies I regret watching: \n \n `)
+// for (let leastFavMovie of leastFavMovies.sort()){
+//     console.log(leastFavMovie)
+// }
 //STEP 9
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+// let allMovies = movies.concat(leastFavMovies)
+// console.log(allMovies.reverse())
 
 //STEP 10
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+// let allMovies = movies.concat(leastFavMovies)
+// //REVERSE ORDER OF CONCATENATED ARRAYS AND RETURN LAST ITEM
+// console.log(allMovies.reverse().pop())
 
 //STEP 11
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+// let allMovies = movies.concat(leastFavMovies)
+// //REVERSE ORDER OF CONCATENATED ARRAYS AND RETURN FIRST ITEM
+// console.log(allMovies.reverse().shift())
 
 //STEP 12
+// let movies = ['Jurassic Park', 'Back to the Future', 'The Rock', 'Ponyo', 'Spiderman across the Spiderverse','Top Gun: Maverick', 'Gladiator']
+// let leastFavMovies = ['Cobweb', 'Super Mario Brothers: The Movie', 'Morbius']
+// let allMovies = movies.concat(leastFavMovies)
+
+// console.log(allMovies.indexOf('Cobweb'))
+// console.log(allMovies.indexOf('Super Mario Brothers: The Movie'))
+// console.log(allMovies.indexOf('Morbius'))
+
+// // let newMovies = allMovies.splice(7,1,'The Dark Knight') && allMovies.splice(8,1,"Avenger's: Endgame") && allMovies.splice(9,1,'Everything Everywhere All At Once')
+// let newMovies = allMovies.splice(7,8,'The Dark Knight',"Avenger's: Endgame",'Everything Everywhere All At Once') 
+// console.log(allMovies)
 
 //STEP 13
-
+// let movies = [['Jurassic Park',1], ['Back to the Future',2], ['The Rock',3], ['Ponyo',4], ['Spiderman across the Spiderverse',5]]
+// let favMovies = movies.filter((movie) =>{
+// console.log(`${movie[0]}`)
+// })
 //STEP 14
+// let employees = ['ZAK', 'JESSICA', 'MARK', 'FRED', 'SALLY']
+
+// let showEmployee = (employees) => {
+// for (let employee of employees){
+//     console.log(employee)
+// }
+// }
+// console.log(`Employees: \n \n `)
+// console.log(showEmployee(employees))
 
 //STEP 15
-
+// let values = [58,'','abcd',true, null,false,0]
+// let filterValues = values.filter((item) => {
+// return typeof item === 'boolean' && item !== false || typeof item === 'number' && item !== 0 || typeof item === 'string' && item !== ''
+  
+// })
+// console.log(filterValues)
 //STEP 16
-
+// let num = [1,2,3,4,5,6,7,8,9,10]
+// function randomNum(num){
+//     return num[Math.floor(Math.random()*num.length)]
+// }
+// console.log(randomNum(num))
 //STEP 17
+// let num = [1,20,3,4,50,6,7,8,9,10]
+// function maxNum(num){
+//     return Math.max(...num)
+// }
+// console.log(maxNum(num))
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,22 +1,34 @@
 // CREATE AN ARRAY OF EMPLOYEES
+let employees = [{id:88888888,name:'Zak', extension:8888, email: 'zak@gmail.com', department: "Administrative"}, 
+{id:11111111,name:'Jessica', extension:1111, email: 'jessica@gmail.com', department: "Engineer"},
+{id:22222222,name:'Mark', extension:2222, email: 'mark@gmail.com', department: "Executive"},
+{id:33333333,name:'Fred', extension:3333, email: 'fred@gmail.com', department: "Marketing"},
+{id:44444444,name:'Sally', extension:4444, email: 'sally@gmail.com', department: "Sales"}]
 
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
-
+sessionStorage.employees = JSON.stringify(employees)
+// console.log(JSON.parse(sessionStorage.getItem('employees')))
 
 // GET DOM ELEMENTS
-
+let form = document.querySelector('form')
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
+let table = document.querySelector('table')
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault()
     // GET THE VALUES FROM THE TEXT BOXES
-
+    let id = document.getElementById('id').value
+    console.log(id)
+    let name = document.getElementById('name').value
+    console.log(name)
+    let ext = document.getElementById('extension').value
+    let email = document.getElementById('email').value
+    let department = document.getElementById('department').value
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
 
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
@@ -24,21 +36,26 @@ form.addEventListener('submit', (e) => {
     // BUILD THE GRID
 
     // RESET THE FORM
-
+    form.reset()
     // SET FOCUS BACK TO THE ID TEXT BOX
-
+    document.getElementById('id').focus()
 });
 
 // DELETE EMPLOYEE
-empTable.addEventListener('click', (e) => {
+table.addEventListener('click', (e) => {
     // CONFIRM THE DELETE
-
+ if (e.target.classList.contains('delete')){
+    if (confirm('Are you sure you want to delete this employee?')){
+       table.deleteRow(e.target.parentElement.parentElement.rowIndex)
+    }
         // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
 
         // REMOVE EMPLOYEE FROM ARRAY
+function clearEmployeeList() {
 
+}
         // BUILD THE GRID
-
+ }
 });
 
 // BUILD THE EMPLOYEES GRID
